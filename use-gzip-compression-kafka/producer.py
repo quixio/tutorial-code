56fc6eeb7917076dc92247081a6b03c53c856a03
Merlin Carter <merlin@lacunalab.org>
2023-01-23 14:55:48 +0700

    updating producer

--- use-gzip-compression-kafka/producer.py
+++ use-gzip-compression-kafka/producer.py
@@ -1,38 +1,36 @@
 import json
 from kafka import KafkaProducer
 
+# Load the source data from the project folder
 file = 'access.log'
 
 producer_compression  = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:json.dumps(x, default=str).encode('utf-8'),
-                         key_serializer=lambda y:str(y).encode("utf-8"),
-                         compression_type="gzip",
-                         linger_ms=1000,
-                         batch_size=10485760)
+                        value_serializer=lambda x:json.dumps(x, default=str).encode('utf-8'),
+                        key_serializer=lambda y:str(y).encode("utf-8"),
+                        compression_type="gzip",
+                        linger_ms=1000,
+                        batch_size=10485760)
 
-producer_topiccompression = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:json.dumps(x, default=str).encode('utf-8'),
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:json.dumps(x,default=str).encode('utf-8'),
                          key_serializer=lambda y:str(y).encode("utf-8"))
 
 
-# ginx-access-log-compressed-trial-1
-#10000
-#10485760
-
-# producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-#                          value_serializer=lambda x:json.dumps(x, default=str).encode('utf-8'),
-#                          key_serializer=lambda y:str(y).encode("utf-8"),
-#                          compression_type="gzip",
-#                          linger_ms=0,
-#                          batch_size=16384)
-
+# Add a switch to let us easily switch between methods
 compmethod = 'topic'
+
+# Initialize a simple counter for the message key
 message_key = 0
+
+# Iterate through file line-by-line
 with open(file, "r") as file_handle:
+
     for line in file_handle:
         print(f"Sending: {line}")
+        # Select the relevant compression method based on the switch
         if compmethod == 'topic':
-            producer_topiccompression.send(topic="nginx-log-topic-compression", value=line, key=message_key)
+            producer.send(topic="nginx-log-topic-compression", value=line, key=message_key)
         else:
-            producer_compression.send(topic="nginx-log-producer-compression ", value=line, key=message_key)
+            producer_compression.send(topic="nginx-log-producer-compression", value=line, key=message_key)
+
         message_key = message_key+1
