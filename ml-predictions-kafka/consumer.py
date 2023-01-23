58bc1db295b43b4baf696a6e66f7eb10ce7608c9
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2023-01-23 14:33:19 +0700

    Update consumer.py

--- ml-predictions-kafka/consumer.py
+++ ml-predictions-kafka/consumer.py
@@ -8,8 +8,7 @@ consumer = KafkaConsumer('predictions',
                          bootstrap_servers=['localhost:9092'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')),
                          auto_offset_reset='earliest',
-                         enable_auto_commit=False
-                         )
+                         enable_auto_commit=False)
 
 for message in consumer:
     print(message.value)
