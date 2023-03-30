88d06c0260486f96f5562e9fb31e35402ca4052c
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2023-03-30 13:58:24 +0200

    Update and rename 2_calculate_distance_km.py to 2_calculate_distance_kms.py

--- reprocess-events/2_calculate_distance_kms.py
+++ reprocess-events/2_calculate_distance_kms.py
@@ -7,47 +7,57 @@ import pandas as pd
 print("Using local kafka")
 client = qx.KafkaStreamingClient('127.0.0.1:9092')
 
-df_i = pd.DataFrame()
+df_dict = {}
 
 def calc_distance(df):
+    if 'distance_cumulative' not in df:
+        cumsum = 0
+    else:
+        cumsum = df['distance_cumulative'].iloc[0]
+
     df['point'] = df.apply(lambda row: Point(latitude=row['latitude'], longitude=row['longitude']), axis=1)
     df['point_prev'] = df['point'].shift(1)
-    df['distance_km'] = df.apply(lambda row: geodesic(row['point'], row['point_prev']).kilometers if row['point_prev'] is not None else float('nan'), axis=1)
-    return df['distance_km']
+    df['distance_kms'] = df.apply(lambda row: geodesic(row['point'], row['point_prev']).kilometers if row['point_prev'] is not None else float('nan'), axis=1)
+    df['distance_cumulative'] = df['distance_kms'].cumsum() + cumsum
 
-#2 — Initialize a Quix Streams consumer to read from the predictions topic (with some extra commit settings)
+    return df['distance_cumulative']
+
+# Initialize a Quix Streams consumer to read from the predictions topic (with some extra commit settings)
 topic_consumer = client.get_topic_consumer("raw-trackpoints", "distance_calculator", auto_offset_reset=qx.AutoOffsetReset.Earliest)
 
-#3 — Initialize a Quix Streams producer for sending predictions to the predictions topic
+# Initialize a Quix Streams producer for sending predictions to the predictions topic
 print("Initializing producer...")
 topic_producer = client.get_topic_producer('distance-calcs')
-output_stream = topic_producer.create_stream()
-
 print(f"Initialized Kafka producer at {dt.datetime.utcnow()}")
 
 
 def on_dataframe_released_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    global df_dict
 
-    global df_i
+    sid = stream_consumer.stream_id
+    print("StreamID: ",sid)
+
+    # Check if the key exists in the df_dict dictionary, if not, initialize it with an empty DataFrame
+    if sid not in df_dict:
+        column_names = ["id","track_id", "distance"]
+        df_dict[sid] = pd.DataFrame(columns=column_names)
+
     # Add last-buffer's last row, to have the previous last location in df
-    df = pd.concat([df_i, df], ignore_index=True)
-
-    # Add distance
+    df = df_dict[sid].append(df)
     df['distance'] = calc_distance(df)
-    latest = df.tail(1)
 
     # Data to output (all rows minus first one, coming from last buffer)
+    output_stream = topic_producer.get_or_create_stream(sid)
+    print("publishing:\n",df[['id','track_id','distance']].iloc[1:].to_markdown())
     output_stream.timeseries.publish(df.iloc[1:])
-    # Print the current total distance traveled
-    print(f"Sending results: | timestamp: [{dt.datetime.utcnow()}] | total distance so far: [{latest['distance'].iloc[0]}.] | device id: [{latest['track_id'].iloc[0]}]  |")
 
     # Update first row for next buffer
-    df_i = df.iloc[[-1]]
+    df_dict[sid] = df.iloc[[-1]]
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     # Create buffer
     buffer = stream_consumer.timeseries.create_buffer()
-    buffer.time_span_in_milliseconds = 10000 # 10 seconds buffer
+    buffer.packet_size = 5 # 10 seconds buffer
     buffer.on_dataframe_released = on_dataframe_released_handler
 
 # Subscribe to new streams being received
