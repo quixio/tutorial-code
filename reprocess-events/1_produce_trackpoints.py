4d23c8493d7db5e41a6e03096440c77892ee638e
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2023-03-30 14:09:55 +0200

    Update 1_produce_trackpoints.py

--- reprocess-events/1_produce_trackpoints.py
+++ reprocess-events/1_produce_trackpoints.py
@@ -4,6 +4,7 @@ import time
 import datetime as dt
 import quixstreams as qx
 
+# Initialize the Quix Streams client for generic Kafka
 print("Using local kafka")
 client = qx.KafkaStreamingClient('127.0.0.1:9092')
 
@@ -11,23 +12,31 @@ client = qx.KafkaStreamingClient('127.0.0.1:9092')
 print("Initializing topic")
 topic_producer = client.get_topic_producer('raw-trackpoints')
 
-
 print(f'Initialized Quix Streams client at {dt.datetime.utcnow()}')
 
 # Read in the CSV file
 df = pd.read_csv("go_track_trackspoints_sm.csv")
 
+# Cycle through all rows in the CSV file and send each row as a message
 for i in range(len(df)):
-    # Create small data frame for each message
+    
+    # Create small data frame for each row
     df_r = df.iloc[[i]]
+    
+    # Fix the time column so it contains unique timestamps (effective UUID for each message)
     df_r["time"] = pd.Timestamp.now()
-    # Print the message so you can see what is being sent
+    
+    # Pretty print the message so you can we see what is being sent
     print("Sending Message: \n", df_r.to_markdown())
 
-    # Send the data with the Quix Streams client
+    # Create separate steams for each device based on "track_id"
     sid = f"device_{df_r['track_id'].iloc[0]}"
     print("StreamID: ", sid)
+    
+    # Get or create a stream depending on whether it exists already
     output_stream = topic_producer.get_or_create_stream(sid)
+    
+    # Publish the message to the stream
     output_stream.timeseries.publish(df_r)
 
     # Optionally wait for a fraction of a second to slow down the stream
